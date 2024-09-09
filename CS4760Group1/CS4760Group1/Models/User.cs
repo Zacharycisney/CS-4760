b70cef1bf76843b89ae2d3a8c6b56840b9ad6558
Zacharycisney <116757784+Zacharycisney@users.noreply.github.com>
2024-09-09 15:25:47 -0600

    Added user class

---
+++ CS4760Group1/CS4760Group1/Models/User.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data;
+namespace CS4760Group1.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public Role Role { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        public string? PhoneNumber { get; set; }
+    }
+
+    // limits allowed roles
+    public enum Role
+    {
+        Admin,
+        Chair,
+        Tenured,    // fully tenured
+        Tenure,     // on track for tenure
+        Instructor,
+        Adjunct,
+        Staff
+    }
+}
--- CS4760Group1/CS4760Group1/obj/Debug/net8.0/CS4760Group1.AssemblyInfo.cs
+++ CS4760Group1/CS4760Group1/obj/Debug/net8.0/CS4760Group1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CS4760Group1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e065df896b5de6e4fa7f5637d49f54dfa331006")]
 [assembly: System.Reflection.AssemblyProductAttribute("CS4760Group1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CS4760Group1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
