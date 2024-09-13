52e3919b4c812315f2bdbf2cb4df733a32c33ece
isabelarmstrong <isabelarmstrong@mail.weber.edu>
2024-09-13 14:41:35 -0600

    Added grant form

---
+++ CS4760Group1/CS4760Group1/Pages/GrantForm.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CS4760Group1.Pages
+{
+    public class GrantFormModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
