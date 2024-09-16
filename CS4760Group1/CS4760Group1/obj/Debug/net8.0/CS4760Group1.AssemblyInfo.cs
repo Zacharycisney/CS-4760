d5b98412e4ccfd330bf5acfd3feb1ce31958bce5
Zacharycisney <116757784+Zacharycisney@users.noreply.github.com>
2024-09-16 09:31:11 -0600

    Cleaned up the home page appearance

--- CS4760Group1/CS4760Group1/obj/Debug/net8.0/CS4760Group1.AssemblyInfo.cs
+++ CS4760Group1/CS4760Group1/obj/Debug/net8.0/CS4760Group1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CS4760Group1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7df78b0da3fb0bebcd395364314dda9aa0ee877")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a17117220eabca26682da932ccffb63bf851270")]
 [assembly: System.Reflection.AssemblyProductAttribute("CS4760Group1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CS4760Group1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
