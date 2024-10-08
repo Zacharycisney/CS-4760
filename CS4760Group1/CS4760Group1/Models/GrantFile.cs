e3d8f00541ee45bbae5bbbfb69251529dfd54527
isabelarmstrong <isabelarmstrong@mail.weber.edu>
2024-10-08 15:18:03 -0600

    Added fix for key not being recognized

--- CS4760Group1/CS4760Group1/Models/GrantFile.cs
+++ CS4760Group1/CS4760Group1/Models/GrantFile.cs
@@ -1,10 +1,14 @@
-﻿namespace CS4760Group1.Models
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace CS4760Group1.Models
 {
     public class GrantFile
     {
-        public int GrantID;
+        [Key]
+        public int FileID { get; set; } //Change this in the db to auto increment
 
-        public int FileID; //Change this in the db to auto increment
+        public int GrantID;
 
         public string? FileName;
 
