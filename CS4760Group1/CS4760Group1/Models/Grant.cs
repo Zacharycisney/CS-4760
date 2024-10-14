b3438c4f35d8a923ced120ed737831808584d5ac
isabelarmstrong <isabelarmstrong@mail.weber.edu>
2024-10-14 16:26:55 -0600

    Added new columns to Grant table

--- CS4760Group1/CS4760Group1/Models/Grant.cs
+++ CS4760Group1/CS4760Group1/Models/Grant.cs
@@ -23,5 +23,23 @@ namespace CS4760Group1.Models
 
         [Required]
         public string? Status { get; set; }
+
+        [Required]
+        public string? SubType { get; set; }
+
+        [Required]
+        public string? Season { get; set; }
+
+        [RegularExpression(@"^(?!0(\.0+)?$)([1-9][0-9]{0,16}|0)(\.[0-9]{1,2})?$"), Required]
+        public decimal AmountFromOther { get; set; }
+
+        [RegularExpression(@"^(?!0(\.0+)?$)([1-9][0-9]{0,16}|0)(\.[0-9]{1,2})?$"), Required]
+        public decimal TotalAmount { get; set; }
+
+        [Required]
+        public int CollegeID { get; set; } //FK to college table
+
+        [Required]
+        public int DepartmentID { get; set; } //FK to dept table
     }
 }
